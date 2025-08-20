4bcc113d6d3396409513456a7dc62bc1b39291c5
Sema <jazz-dev@jazz-soft.net>
2025-08-19 21:50:34 -0400

    placeholder

---
+++ javascript/JZZ.midi.RTC.js
@@ -0,0 +1,17 @@
+(function(global, factory) {
+  /* istanbul ignore next */
+  if (typeof exports === 'object' && typeof module !== 'undefined') {
+    factory.RTC = factory;
+    module.exports = factory;
+  }
+  else if (typeof define === 'function' && define.amd) {
+    define('JZZ.midi.RTC', ['JZZ'], factory);
+  }
+  else {
+    factory(JZZ);
+  }
+})(this, function(JZZ) {
+
+  /* istanbul ignore next */
+  if (JZZ.RTC) return;
+});
