5764b12a58cae57a89073d163b9bc6c272c102f3
Sema <jazz-dev@jazz-soft.net>
2025-08-24 20:55:27 -0400

    Server part

--- README.md
+++ README.md
@@ -2,3 +2,62 @@
 ## MIDI via WebRTC
 
 coming soon...
+
+[![npm](https://img.shields.io/npm/v/jzz-midi-rtc.svg)](https://www.npmjs.com/package/jzz-midi-rtc)
+[![npm](https://img.shields.io/npm/dt/jzz-midi-rtc.svg)](https://www.npmjs.com/package/jzz-midi-rtc)
+[![build](https://github.com/jazz-soft/JZZ-midi-RTC/actions/workflows/build.yml/badge.svg)](https://github.com/jazz-soft/JZZ-midi-RTC/actions)
+[![Coverage Status](https://coveralls.io/repos/github/jazz-soft/JZZ-midi-RTC/badge.svg)](https://coveralls.io/github/jazz-soft/JZZ-midi-RTC)
+
+## Usage
+##### Plain HTML
+```html
+<script src="JZZ.js"></script>
+<script src="JZZ.midi.RTC.js"></script>
+//...
+```
+##### CDN (jsdelivr)
+```html
+<script src="https://cdn.jsdelivr.net/npm/jzz"></script>
+<script src="https://cdn.jsdelivr.net/npm/jzz-midi-rtc"></script>
+//...
+```
+##### CDN (unpkg)
+```html
+<script src="https://unpkg.com/jzz"></script>
+<script src="https://unpkg.com/jzz-midi-rtc"></script>
+//...
+```
+##### CommonJS
+```js
+var JZZ = require('jzz');
+require('jzz-midi-rtc')(JZZ);
+//...
+```
+
+## Example
+```js
+var MidiRTC = new JZZ.RTC();
+var RTCPC = new RTCPeerConnection();
+// ...
+// ... boring boilerplate WebRTC code here ...
+// ...
+MidiRTC.connect(RTCPC);
+```
+
+## API
+```js
+var MidiRTC = new JZZ.RTC();
+```
+Constructor.
+
+```js
+MidiRTC.addMidiIn(name, port);
+MidiRTC.addMidiOut(name, port);
+```
+Add MIDI port. `name` - name to bee seen by the remotr peer; `port` - MIDI port (real or virtual).
+
+```js
+MidiRTC.removeMidiIn(name);
+MidiRTC.removeMidiOut(name);
+```
+Remove MIDI port.
--- javascript/JZZ.midi.RTC.js
+++ javascript/JZZ.midi.RTC.js
@@ -15,43 +15,95 @@
   /* istanbul ignore next */
   if (JZZ.RTC) return;
 
-  function connect(rtc) {
-    var self = new JZZ.lib.R();
-    var ins = {};
-    var outs = {};
-    var inputs = [];
-    var outputs = [];
+  function RTC() {
+    this.ins = {};
+    this.outs = {};
+    this.inputs = [];
+    this.outputs = [];
+  }
 
-    const channel = rtc.createDataChannel('MIDI');
-    channel.addEventListener('open', function(evt) {
-      channel.send("Hi you!");
+  RTC.prototype.connect = function(rtc) {
+    var self = this;
+    var chan = rtc.createDataChannel('MIDI');
+    this.chan = chan;
+    chan.addEventListener('open', function(evt) {
+      _send(self, _info(self));
     });
-    channel.addEventListener('message', function(evt) {
-      console.log('local:', evt.data);
+    chan.addEventListener('close', function(evt) {
+      self.chan = undefined;
     });
-
+    chan.addEventListener('message', function(evt) {
+      try {
+        var x = JSON.parse(evt.data);
+        if (x.midi) {
+          if (self.outs[x.id]) self.outs[x.id].send(_decode(x.midi));
+        }
+      }
+      catch(e) {/**/}
+    });
     rtc.addEventListener('datachannel', function(evt) {
       const channel = evt.channel;
       if (channel.label != 'MIDI') return;
       channel.addEventListener('open', function(evt) {
-        channel.send("Hi back!");
+        //channel.send("Hi back!");
       });
       channel.addEventListener('message', function(evt) {
-        console.log('remote:', evt.data);
+        console.log(evt.data);
       });
     });
+  };
 
-    self.addMidiIn = function(name, widget) {
-    };
-    self.addMidiOut = function(name, widget) {
-    };
-    self.removeMidiIn = function(name) {
-    };
-    self.removeMidiOut = function(name) {
-    };
-    return self._thenable();
+  RTC.prototype.close = function() {
+  };
+
+  RTC.prototype.addMidiIn = function(name, widget) {
+    if (this.ins[name]) return;
+    this.ins[name] = widget;
+    this.inputs.push(name);
+    var self = this;
+    widget.connect(function(msg) {
+      _send(self, JSON.stringify({ id: name, midi: _encode(msg) }));
+    });
+    _send(this, _info(this));
+  };
+  RTC.prototype.addMidiOut = function(name, widget) {
+    if (this.outs[name]) return;
+    this.outs[name] = widget;
+    this.outputs.push(name);
+    _send(this, _info(this));
+  };
+  RTC.prototype.removeMidiIn = function(name) {
+    var n = this.inputs.indexOf(name);
+    if (n != -1) {
+      this.inputs.splice(n, 1);
+      this.ins[name].disconnect();
+      delete this.ins[name];
+      _send(this, _info(this));
+    }
+  };
+  RTC.prototype.removeMidiOut = function(name) {
+    var n = this.outputs.indexOf(name);
+    if (n != -1) {
+      this.outputs.splice(n, 1);
+      this.outs[name].disconnect();
+      delete this.outs[name];
+      _send(this, _info(this));
+    }
+  };
+  function _add(a, x) {
+    for (var n = 0; n < a.length; n++) if (a[n] == x) return a;
+    a.push(x);
+    return a;
   }
-
+  function _remove(a, x) {
+    var v = [];
+    for (var n = 0; n < a.length; n++) if (a[n] != x) v.push(x);
+    return v;
+  }
+  function _info(self) {
+    return JSON.stringify({ info: { inputs: self.inputs, outputs: self.outputs }});
+  }
+  function _send(self, msg) { if (self.chan) self.chan.send(msg); }
   function _encode(m) {
     var x = { midi: m.splice(0, m.length) };
     var k = Object.keys(m);
@@ -64,10 +116,12 @@
     for (var n = 0; n < k.length; n++) if (k[n] != 'midi') m[k[n]] = k[k[n]];
     return m;
   }
+  function _diff(a, b) {
+    var i, aa = [], bb = [];
+    for (i = 0; i < a.length; i++) if (!b.includes(a[i])) aa.push(a[i]);
+    for (i = 0; i < b.length; i++) if (!a.includes(b[i])) bb.push(b[i]);
+    return [aa, bb];
+  }
 
-  JZZ.RTC = {
-    connect: connect,
-    decode: _decode,
-    encode: _encode
-  };
+  JZZ.RTC = RTC;
 });
