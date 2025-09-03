97bf2609d849db8cfa99b213e08f6eb219b73f74
Sema <jazz-dev@jazz-soft.net>
2025-09-02 20:43:44 -0400

    Fake WebRTC

--- test/mocha.js
+++ test/mocha.js
@@ -2,6 +2,24 @@ var assert = require('assert');
 var JZZ = require('jzz');
 require('..')(JZZ);
 
+function Peer() {
+  this.other = undefined;
+}
+Peer.prototype.connect = function(P) {
+  this.other = P;
+  P.other = this;
+};
+Peer.prototype.createDataChannel = function() {
+  return new Chan();
+};
+Peer.prototype.addEventListener = function() {
+};
+
+function Chan() {
+}
+Chan.prototype.addEventListener = function() {
+};
+
 describe('WebRTC', function() {
   var RTC0 = new JZZ.RTC();
   var RTC1 = new JZZ.RTC('WebRTC1');
@@ -11,5 +29,9 @@ describe('WebRTC', function() {
     assert.equal(RTC0.pref, 'WebRTC');
     assert.equal(RTC1.pref, 'WebRTC1');
     assert.equal(RTC2.pref, 'WebRTC2');
+  });
+  it('connect', function() {
+    var rtc = new Peer();
+    RTC0.connect(rtc);
   });
 });
