fbd432cdbcab555b68618b00f7b1a2c1318e3683
Sema <jazz-dev@jazz-soft.net>
2025-09-03 21:24:08 -0400

    Test - in progress

--- test/mocha.js
+++ test/mocha.js
@@ -2,36 +2,58 @@ var assert = require('assert');
 var JZZ = require('jzz');
 require('..')(JZZ);
 
+// fake WebRTC connection
 function Peer() {
+  this.CH = {};
+  this.EH = {};
   this.other = undefined;
 }
 Peer.prototype.connect = function(P) {
   this.other = P;
   P.other = this;
+  this.connected();
+  P.connected();
 };
-Peer.prototype.createDataChannel = function() {
-  return new Chan();
+Peer.prototype.connected = function() {
+  if (this.EH['datachannel']) {
+  }
 };
-Peer.prototype.addEventListener = function() {
+Peer.prototype.createDataChannel = function(lab) {
+  return new Chan(lab);
+};
+Peer.prototype.addEventListener = function(evt, fun) {
+  // console.log('Peer.addEventListener', evt, fun);
+  if (!this.EH[evt]) this.EH[evt] = [];
+  this.EH[evt].push(fun);
 };
 
-function Chan() {
+function Chan(lab) {
+  this.label = lab;
+  this.EH = {};
 }
-Chan.prototype.addEventListener = function() {
+Chan.prototype.addEventListener = function(evt, fun) {
+  // console.log('Chan.addEventListener', evt, fun);
+  if (!this.EH[evt]) this.EH[evt] = [];
+  this.EH[evt].push(fun);
 };
 
 describe('WebRTC', function() {
   var RTC0 = new JZZ.RTC();
   var RTC1 = new JZZ.RTC('WebRTC1');
   var RTC2 = new JZZ.RTC();
+  var RTC3 = new JZZ.RTC();
+  var Peer0 = new Peer();
+  var Peer1 = new Peer();
 
   it('constructor', function() {
     assert.equal(RTC0.pref, 'WebRTC');
     assert.equal(RTC1.pref, 'WebRTC1');
     assert.equal(RTC2.pref, 'WebRTC2');
+    assert.equal(RTC3.pref, 'WebRTC3');
   });
   it('connect', function() {
-    var rtc = new Peer();
-    RTC0.connect(rtc);
+    RTC0.connect(Peer0);
+    RTC1.connect(Peer1);
+    Peer0.connect(Peer1);
   });
 });
