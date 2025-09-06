e2ab6e76c5ef9293830b721ebd431e44c6db2041
Sema <jazz-dev@jazz-soft.net>
2025-09-06 19:39:07 -0400

    Updated dependencies

--- test/mocha.js
+++ test/mocha.js
@@ -4,22 +4,37 @@ require('..')(JZZ);
 
 // fake WebRTC connection
 function Peer() {
-  this.CH = {};
+  this.CL = [];
+  this.CR = [];
   this.EH = {};
   this.other = undefined;
 }
-Peer.prototype.connect = function(P) {
-  this.other = P;
-  P.other = this;
-  this.connected();
-  P.connected();
+Peer.prototype.connect = function(R) {
+  var L = this;
+  if (L.other || R.other) throw 'Already connected';
+  L.other = R;
+  R.other = L;
+  var c;
+  for (c of L.CL) R.CR.push(c.other);
+  for (c of R.CL) L.CR.push(c.other);
+  setTimeout(function() {
+    L.connected();
+    R.connected();
+  }, 0);
 };
 Peer.prototype.connected = function() {
-  if (this.EH['datachannel']) {
-  }
+  var f, c;
+  if (this.EH['datachannel']) for (f of this.EH['datachannel']) for (c of this.CR) f({ channel: c });
 };
 Peer.prototype.createDataChannel = function(lab) {
-  return new Chan(lab);
+  var c = new Chan(lab);
+  c.other = new Chan(lab);
+  c.other.other = c;
+  this.CL.push(c);
+  if (this.other) {
+    this.other.CR.push(c.other);
+  }
+  return c;
 };
 Peer.prototype.addEventListener = function(evt, fun) {
   // console.log('Peer.addEventListener', evt, fun);
@@ -30,6 +45,7 @@ Peer.prototype.addEventListener = function(evt, fun) {
 function Chan(lab) {
   this.label = lab;
   this.EH = {};
+  this.other = undefined;
 }
 Chan.prototype.addEventListener = function(evt, fun) {
   // console.log('Chan.addEventListener', evt, fun);
